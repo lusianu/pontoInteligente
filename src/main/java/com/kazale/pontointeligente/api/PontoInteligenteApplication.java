923ef2d7f3adb44f836987d2b897c02af9777104
luciano <januarioluciano@gmail.com>
2019-11-12 00:26:03 -0300

    first commit

---
+++ src/main/java/com/kazale/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,14 @@
+package com.kazale.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+}
